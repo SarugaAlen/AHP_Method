f0c2d72517aa2a345c7db2c6b20495e3a284961a
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2023-05-13 19:40:26 +0200

    added tree structure

--- AHP_Method/MainWindow.xaml.cs
+++ AHP_Method/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AHP_Method.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,65 @@ namespace AHP_Method
     /// </summary>
     public partial class MainWindow : Window
     {
+        Parameter rootParameter = new Parameter("Problem odločanja");
         public MainWindow()
         {
             InitializeComponent();
+            var rootCollection = new ObservableCollection<Parameter> { rootParameter };
+            DataContext = rootCollection;
+        }
+
+
+
+        private void dodajParameter_Click(object sender, RoutedEventArgs e)
+        {
+            string newParameterName = newParameterTextBox.Text;
+            if (!String.IsNullOrEmpty(newParameterName))
+            {
+                Parameter newChild = new Parameter(newParameterName);
+                if (treeView.SelectedItem != null)
+                {
+                    Parameter selectedParameter = treeView.SelectedItem as Parameter;
+                    if (selectedParameter != null)
+                    {
+                        selectedParameter.Children.Add(newChild);
+                        newChild.Parent = selectedParameter;
+                    }
+                }
+                else
+                {
+                    rootParameter.Children.Add(newChild);
+                    newChild.Parent = rootParameter;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vnesite ime parametra!");
+            }
+            newParameterTextBox.Text = "";
+        }
+
+        private void odstraniParameter_Click(object sender, RoutedEventArgs e)
+        {
+            Parameter selectedParameter = treeView.SelectedItem as Parameter;
+            if (selectedParameter != null && selectedParameter != rootParameter)
+            {
+                if (selectedParameter.Parent != null)
+                {
+                    selectedParameter.Parent.Children.Remove(selectedParameter);
+                }
+                else
+                {
+                    rootParameter.Children.Remove(selectedParameter);
+                }
+
+                foreach (Parameter child in selectedParameter.Children)
+                {
+                    child.Parent = null;
+                }
+
+                ((ObservableCollection<Parameter>)DataContext).Remove(selectedParameter);
+            }
         }
     }
 }
---
+++ AHP_Method/Model/Parameter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHP_Method.Model
+{
+    internal class Parameter
+    {
+        public string Name { get; set; }
+        public Parameter Parent { get; set; }
+        private ObservableCollection<Parameter> _children;
+        public ObservableCollection<Parameter> Children
+        {
+            get { return _children; }
+            set
+            {
+                _children = value;
+                OnPropertyChanged(nameof(Children));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public Parameter()
+        {
+            Children = new ObservableCollection<Parameter>();
+
+        }
+        
+        public Parameter(string name)
+        {
+            Name = name;
+            Children = new ObservableCollection<Parameter>();
+        }
+    }
+}
